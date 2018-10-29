5aaf57c1b08cb87580a8ec2a9aa0bdc409c85621
Taras Vitovshchyk <vitovstreet@gmail.com>
2018-10-29 17:26:18 +0200

    added ZipCode.vue and form markup

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,22 @@
+<style lang="sass">
+    @import url('https://use.fontawesome.com/releases/v5.3.1/css/all.css')
+    @import "./assets/style/main.sass"
+    @import "./assets/style/media.sass"
+</style>
 <template>
   <div id="app">
-    <Users/>
+    <zip-code></zip-code>
   </div>
 </template>
 
 <script>
+import ZipCode from './components/ZipCode.vue'
+
 
 export default {
   name: 'app',
   components: {
-
+  	ZipCode,
   },
 };
 </script>
---
+++ src/components/ZipCode.vue
@@ -0,0 +1,27 @@
+<template>
+	<div class="container">
+		<h1>Zip Code test</h1>
+		<form>
+			<div class="input_cell">
+				<input type="text" placeholder="Enter zip code" class="form_field" v-model="zip">
+			</div>
+			<button class="btn form_btn">Add location</button>
+		</form>
+	</div>
+</template>
+
+
+
+<script>
+	
+
+export default {
+	name: 'ZipCode',
+	data() {
+		return {
+
+		}
+	}
+}
+
+</script>
